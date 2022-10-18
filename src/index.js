a67450c75e4bc83d991010be0cba3c1002f8995d
quirogak <quirogakill@gmail.com>
2022-10-18 16:46:13 -0500

    Removed folder "font" and generated home elements.

--- src/index.js
+++ src/index.js
@@ -13,12 +13,6 @@ const staticElements = (() => {
   mainContainer.appendChild(footer)
 
 
-  //header content
-  const title = document.createElement("h1")
-  title.setAttribute("class","main-title")
-  title.textContent = "Planeta Limón"
-  header.appendChild(title)
-
   const navBar = document.createElement("div")
   navBar.setAttribute("class", "nav-bar")
   header.appendChild(navBar)
@@ -72,27 +66,28 @@ const genElements = (() => {
  const button2 =  document.querySelector("#button2")
  const button3 =  document.querySelector("#button3")
 
+
   const buttonSwitch = (e) => {
 
 
    if (e.target.id == "button1" ){
 
-    button1.setAttribute('style', 'border: 2px solid rgb(243, 226, 131);')
-    button2.setAttribute('style', 'border: none;')
-    button3.setAttribute('style', 'border: none;')
+    button1.setAttribute('style', 'border: 4px solid rgb(241, 209, 28);')
+    button2.setAttribute('style', 'border: 2px solid black;')
+    button3.setAttribute('style', 'border: 2px solid black;')
    }
 
    
    if (e.target.id == "button2" ){
-    button2.setAttribute('style', 'border: 2px solid rgb(243, 226, 131);')
-    button1.setAttribute('style', 'border: none;')
-    button3.setAttribute('style', 'border: none;')
+    button2.setAttribute('style', 'border: 4px solid rgb(241, 209, 28);')
+    button1.setAttribute('style', 'border: 2px solid black;')
+    button3.setAttribute('style', 'border: 2px solid black;')
    }
    
    if (e.target.id == "button3" ){
-    button3.setAttribute('style', 'border: 2px solid rgb(243, 226, 131);')
-    button2.setAttribute('style', 'border: none;')
-    button1.setAttribute('style', 'border: none;')
+    button3.setAttribute('style', 'border: 4px solid rgb(241, 209, 28);')
+    button2.setAttribute('style', 'border: 2px solid black;')
+    button1.setAttribute('style', 'border: 2px solid black;')
    }
    
 
@@ -102,12 +97,60 @@ const genElements = (() => {
 
   const genHome = () => {
 
+    //title
+
+    const titleContainer = document.createElement("div")
+    titleContainer.setAttribute("class","generic-container")
+    titleContainer.setAttribute("id","home-title")
+    document.querySelector(".main-content").appendChild(titleContainer)
+    
+    const leafIcon = document.createElement("img")
+    leafIcon.src ="../img/Leaf_icon_03.svg.png"
+    leafIcon.setAttribute("class","leaf-icon")
+    titleContainer.appendChild(leafIcon)
+
+    const restaurantTitle = document.createElement("h2")
+    restaurantTitle.setAttribute("class","res-title")
+    titleContainer.appendChild(restaurantTitle)
+    restaurantTitle.textContent = "Planeta Limón"
+
+    //description
+
+    const descriptionContainer = document.createElement("div")
+    descriptionContainer.setAttribute("class","generic-container")
+    descriptionContainer.setAttribute("id","desc-text")
+    document.querySelector(".main-content").appendChild(descriptionContainer)
+
+
+    const descriptionText = document.createElement("p")
+    descriptionText.setAttribute("class","p-text")
+    descriptionContainer.appendChild(descriptionText)
+    descriptionText.textContent = "Welcome to Planeta Limón, the worlds most famous lemon-based restaurant, where every meal are made of natural and delicious lemons. Come and enjoy this acid but delicious flavoured adventure!"
+
+    const descriptionPhrase = document.createElement("p")
+    descriptionPhrase.setAttribute("class","p-phrase")
+    descriptionContainer.appendChild(descriptionPhrase)
+    descriptionPhrase.textContent = "When life gives you lemons, visit Planeta Limón and get a discount!"
+
+    const lemonIcon = document.createElement("img")
+    lemonIcon.src ="../img/lemon-icon.png"
+    lemonIcon.setAttribute("class","lemon-icon")
+    descriptionContainer.appendChild(lemonIcon)
+  
+
+  
+
 
   }
 
+ 
+//eventListeners
 
-
+  window.onload = () => {
+    button1.click();
+  }
   button1.addEventListener("click", buttonSwitch)
+  button1.addEventListener("click", genHome)
 
   button2.addEventListener("click", buttonSwitch)
 
