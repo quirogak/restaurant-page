cc5757ebfe2e345649df1d4f7527ec622db33e30
quirogak <quirogakill@gmail.com>
2022-10-26 13:25:49 -0500

    Finished the contacts section.

--- src/index.js
+++ src/index.js
@@ -89,7 +89,7 @@ const genElements = (() => {
    }
    
    if (e.target.id == "button3" ){
-    
+
     document.querySelector(".main-content").textContent = ""
     button3.setAttribute('style', 'border: 4px solid rgb(241, 209, 28);')
     button2.setAttribute('style', 'border: 2px solid black;')
@@ -116,7 +116,7 @@ const genElements = (() => {
     titleContainer.appendChild(leafIcon)
 
     const restaurantTitle = document.createElement("h2")
-    restaurantTitle.setAttribute("class","res-title")
+    restaurantTitle.setAttribute("class","title")
     titleContainer.appendChild(restaurantTitle)
     restaurantTitle.textContent = "Planeta Limón"
 
@@ -189,9 +189,171 @@ const genElements = (() => {
     leafIcon2.setAttribute("id","leaf-icon2")
     locationContainer.appendChild(leafIcon2)
 
+  }
+
+  const genMenu = () => {
+
+    const titleContainer = document.createElement("div")
+    titleContainer.setAttribute("class","generic-container")
+    titleContainer.setAttribute("id","menu-title-container")
+    document.querySelector(".main-content").appendChild(titleContainer)
+    
+    const leafIcon = document.createElement("img")
+    leafIcon.src ="../img/leaf-icon.png"
+    leafIcon.setAttribute("class","leaf-icon")
+    titleContainer.appendChild(leafIcon)
+
+    const menuTitle = document.createElement("h2")
+    menuTitle.setAttribute("class","title")
+    titleContainer.appendChild(menuTitle)
+    menuTitle.textContent = "Menu"
+
+
+
+  }
+
+  const genContact = () => {
+
+    const titleContainer = document.createElement("div")
+    titleContainer.setAttribute("class","generic-container")
+    titleContainer.setAttribute("id","contact-title-container")
+    document.querySelector(".main-content").appendChild(titleContainer)
+    
+    const leafIcon = document.createElement("img")
+    leafIcon.src ="../img/leaf-icon.png"
+    leafIcon.setAttribute("class","leaf-icon")
+    leafIcon.setAttribute("id","leaf-icon3")
+    titleContainer.appendChild(leafIcon)
+
+    const contactTitle = document.createElement("h2")
+    contactTitle.setAttribute("class","title")
+    contactTitle.setAttribute("id","contact-title")
+    titleContainer.appendChild(contactTitle)
+    contactTitle.textContent = "Contact us"
+
+    /*----------------------------------------------------*/ 
+
+    const contactOne = document.createElement("div")
+    contactOne.setAttribute("class","generic-container")
+    contactOne.setAttribute("id","contact-1")
+    document.querySelector(".main-content").appendChild(contactOne)
+
+    const infoContainer = document.createElement("div")
+    infoContainer.setAttribute("class","info-container")
+    contactOne.appendChild(infoContainer)
+
+    const contactSubTitle = document.createElement("h3")
+    contactSubTitle.setAttribute("class","name")
+    infoContainer.appendChild(contactSubTitle)
+    contactSubTitle.textContent = "Clank"
+
+    const contactDetails = document.createElement("p")
+    contactDetails.setAttribute("class","details")
+    infoContainer.appendChild(contactDetails)
+    contactDetails.textContent = "CEO"
+
+    const contactNumber = document.createElement("p")
+    contactNumber.setAttribute("class","details")
+    infoContainer.appendChild(contactNumber)
+    contactNumber.textContent = " +57 324112333 "
+
+    const contactEmail = document.createElement("p")
+    contactEmail.setAttribute("class","details")
+    infoContainer.appendChild(contactEmail)
+    contactEmail.textContent = "EmailRandomExample@gmail.com"
+
+    const contactPhoto = document.createElement("img")
+    contactPhoto.src = "../img/clank.png"
+    contactPhoto.setAttribute("class","profile-pic")
+    contactOne.appendChild(contactPhoto)
+
+
 
   
+    /*----------------------------------------------------*/ 
 
+
+
+    /*----------------------------------------------------*/ 
+
+    const contactTwo = document.createElement("div")
+    contactTwo.setAttribute("class","generic-container")
+    contactTwo.setAttribute("id","contact-2")
+    document.querySelector(".main-content").appendChild(contactTwo)
+
+    const infoContainer2 = document.createElement("div")
+    infoContainer2.setAttribute("class","info-container")
+    contactTwo.appendChild(infoContainer2)
+
+    const contactSubTitle2 = document.createElement("h3")
+    contactSubTitle2.setAttribute("class","name")
+    infoContainer2.appendChild(contactSubTitle2)
+    contactSubTitle2.textContent = "Sackboy"
+
+    const contactDetails2 = document.createElement("p")
+    contactDetails2.setAttribute("class","details")
+    infoContainer2.appendChild(contactDetails2)
+    contactDetails2.textContent = "Chef"
+
+    const contactNumber2 = document.createElement("p")
+    contactNumber2.setAttribute("class","details")
+    infoContainer2.appendChild(contactNumber2)
+    contactNumber2.textContent = " +57 325123222"
+
+    const contactEmail2 = document.createElement("p")
+    contactEmail2.setAttribute("class","details")
+    infoContainer2.appendChild(contactEmail2)
+    contactEmail2.textContent = "EmailRandomExample@gmail.com"
+
+    const contactPhoto2 = document.createElement("img")
+    contactPhoto2.src = "../img/sackboy.png"
+    contactPhoto2.setAttribute("class","profile-pic")
+    contactTwo.appendChild(contactPhoto2)
+
+
+  
+  
+    /*----------------------------------------------------*/ 
+
+
+
+    /*----------------------------------------------------*/ 
+
+    const contactThree = document.createElement("div")
+    contactThree.setAttribute("class","generic-container")
+    contactThree.setAttribute("id","contact-3")
+    document.querySelector(".main-content").appendChild(contactThree)
+
+    const infoContainer3 = document.createElement("div")
+    infoContainer3.setAttribute("class","info-container")
+    contactThree.appendChild(infoContainer3)
+
+    const contactSubTitle3 = document.createElement("h3")
+    contactSubTitle3.setAttribute("class","name")
+    infoContainer3.appendChild(contactSubTitle3)
+    contactSubTitle3.textContent = "Birdy"
+
+    const contactDetails3 = document.createElement("p")
+    contactDetails3.setAttribute("class","details")
+    infoContainer3.appendChild(contactDetails3)
+    contactDetails3.textContent = "Receptionist"
+
+    const contactNumber3 = document.createElement("p")
+    contactNumber3.setAttribute("class","details")
+    infoContainer3.appendChild(contactNumber3)
+    contactNumber3.textContent = " +34 124112737 "
+
+    const contactEmail3 = document.createElement("p")
+    contactEmail3.setAttribute("class","details")
+    infoContainer3.appendChild(contactEmail3)
+    contactEmail3.textContent = "EmailRandomExample@gmail.com"
+
+    const contactPhoto3 = document.createElement("img")
+    contactPhoto3.src = "../img/birdy.png"
+    contactPhoto3.setAttribute("class","profile-pic")
+    contactThree.appendChild(contactPhoto3)
+  
+    /*----------------------------------------------------*/ 
 
   }
 
@@ -201,12 +363,17 @@ const genElements = (() => {
   window.onload = () => {
     button1.click();
   }
+
   button1.addEventListener("click", buttonSwitch)
   button1.addEventListener("click", genHome)
 
   button2.addEventListener("click", buttonSwitch)
+  button2.addEventListener("click", genMenu)
 
   button3.addEventListener("click", buttonSwitch)
+  button3.addEventListener("click", genContact)
+
+  
 
 })();
 
